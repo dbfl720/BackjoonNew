ec19a277eb374323b694a37452655719443cf0d5
YURI HONG <dbfl720@gmail.com>
2023-04-07 15:28:50 +0900

    no message

---
+++ src/Backjoon/day03.java
@@ -0,0 +1,22 @@
+package Backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class day03 {
+
+	public static void main(String[] args) throws IOException {
+		// 단어 공부
+		// 가장 많이 사용된 알파벳이 무엇인지 알아내는 프로그램
+		// 장 많이 사용된 알파벳을 대문자로 출력한다. 단, 가장 많이 사용된 알파벳이 여러 개 존재하는 경우에는 ?를 출력
+		
+		BufferedReader br = new BufferedReader (new InputStreamReader (System.in));
+		String str = br.readLine();
+		
+		int[] arr = 
+ 		
+	
+	}
+
+}
